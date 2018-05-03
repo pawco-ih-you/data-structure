8fac5e63aacc8f425b4564148f2742d50a77d9da
you ilhwan <nonownonow@gmail.com>
2018-05-04 06:47:17 +0900

    [add-test] queues.spec

---
+++ test/Queues.spec.js
@@ -0,0 +1,25 @@
+const Queue = require('../src/Queue')
+
+describe('Queue', () => {
+  describe('constructor', () => {
+    it('init a dataStore by assigning empty array ')
+  })
+  describe('enqueue', () => {
+    it('adds an element to the end of a queue')
+  })
+  describe('dequeue', () => {
+    it('removes an element from the front of a queue')
+  })
+  describe('front', () => {
+    it('examines the front element of a queue')
+  })
+  describe('back', () => {
+    it('examines the back element of a queue')
+  })
+  describe('toString', () => {
+    it('displays all the elements in a queue')
+  })
+  describe('empty', () => {
+    it('lets we know if a queue is empty')
+  })
+})
